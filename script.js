5869ce4363f5b3b9ceb194f11b176747890aa6b1
rachelm <rmcallister987@gmail.com>
2023-04-29 19:47:34 -0500

    created files and added psuedo code

---
+++ script.js
@@ -0,0 +1,12 @@
+// add the API URL and key
+//connect to the form element in the HTML page and I need to run the search function
+// I need to connect to the items I want to output to on the HTML
+
+//Once I've connected to all of those and created the search function, then I will need to create a function to display the elemnts and the needed info on the HTML page. 
+// I need the function to find the forcast elemtents 
+// I need to connect to the page elemnts that will display the forecast and display elemtents 
+// Save the searches to local storage 
+// read from local storage to add those elemnents to the page 
+// connecting the previous searches to rerun  if they are clicked on 
+
+// add CSS after the fact to display everything 
