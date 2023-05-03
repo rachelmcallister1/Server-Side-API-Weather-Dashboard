dbe19f1f7adafec7e68c0988ce326efe96ab3d89
rachelm <rmcallister987@gmail.com>
2023-05-02 20:00:18 -0500

    added text

--- script.js
+++ script.js
@@ -33,16 +33,16 @@ function fetchWeather(city) {
         })
         .then(data => {
             cityNameEl.innerText= data.city.name //adding name of city on screen 
-            cityTempEl.innerText= data.list[0].main.temp 
-            cityWindEl.innerText= data.list[0].wind.speed 
-            cityHumidityEl.innerText= data.list[0].main.humidity + "string"
+            cityTempEl.innerText= data.list[0].main.temp  + "Fahrenheit"
+            cityWindEl.innerText= data.list[0].wind.speed + "MPH"
+            cityHumidityEl.innerText= data.list[0].main.humidity + "%"
             console.log(data)
             var index=1
             for (i=7; i<data.list.length; i=i+8){
-                document.getElementById("date"+index).innerText=data.list[i].dt
-                document.getElementById("temp"+index).innerText=data.list[i].main.temp
-                document.getElementById("wind"+index).innerText=data.list[i].wind.speed
-                document.getElementById("wind"+index).innerText=data.list[i].main.humidity
+                document.getElementById("date"+index).innerText=data.list[i].dt 
+                document.getElementById("temp"+index).innerText=data.list[i].main.temp + " Fahrenheit"
+                document.getElementById("wind"+index).innerText=data.list[i].wind.speed + " MPH"
+                document.getElementById("wind"+index).innerText=data.list[i].main.humidity + " %"
                 index++ //add dayjs after innertext
             }
         }) //putting in curly brackets so we can do more than one line 
